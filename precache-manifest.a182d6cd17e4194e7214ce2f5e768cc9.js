ab5beedba6af2cdcd609be38dbfff8c5b40587ca
Vasyl-Hnatyshyn <gnatv7077@gmail.com>
2020-05-02 12:16:30 +0300

    Updates

---
+++ precache-manifest.a182d6cd17e4194e7214ce2f5e768cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859497f41f3bb91041725da2d230e077",
+    "url": "/Pokemons-/index.html"
+  },
+  {
+    "revision": "faf42514669e5f87fb24",
+    "url": "/Pokemons-/static/css/main.4fbadbff.chunk.css"
+  },
+  {
+    "revision": "412dbc3c453c87baeae0",
+    "url": "/Pokemons-/static/js/2.4c58b538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemons-/static/js/2.4c58b538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf42514669e5f87fb24",
+    "url": "/Pokemons-/static/js/main.00ee00fa.chunk.js"
+  },
+  {
+    "revision": "9d981e65c34f8e8b5a0f",
+    "url": "/Pokemons-/static/js/runtime-main.b5428441.js"
+  }
+]);
